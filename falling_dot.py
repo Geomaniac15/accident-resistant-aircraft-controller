0d7b246ef551c6e2c9d485b74f9a80ee2f5c25d9
George <gveal07@outlook.com>
2026-06-07 14:57:15 +0100

    added throttle for altitude control

--- falling_dot.py
+++ falling_dot.py
@@ -1,13 +1,19 @@
 dt = 0.01   # time per tick, in seconds
 g = 9.81    # acceleration due to gravity, in m/s^2
+mass = 1.0    # mass of the dot, in kg
 
 height = 100.0  # initial height, in meters
-velocity = 20.0  # initial velocity, in m/s
+velocity = 0.0 # initial velocity, in m/s
+
+throttle = g  # upward force in Newtons
 
 t = 0.0  # initial time, in seconds
-while height > 0:
-    # dynamics
-    velocity = velocity - g * dt
+while height > 0 and t < 30.0:
+    # net force: thrust - weight
+    net_force = throttle - mass * g
+    acceleration = net_force / mass
+    
+    velocity = velocity + acceleration * dt
     height = height + velocity * dt
 
     t = t + dt
