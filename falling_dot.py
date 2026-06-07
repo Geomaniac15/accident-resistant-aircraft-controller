f31239d03650ab37c2d79aca1988de4aec2658f7
George <gveal07@outlook.com>
2026-06-07 15:18:49 +0100

    limited throttle so it cant go negative and fixed Kd so it doesnt overshoot anymore

--- falling_dot.py
+++ falling_dot.py
@@ -11,9 +11,10 @@ target = 100.0  # target height, in meters
 hover = mass * g  # thrust required to hover, in Newtons
 
 Kp = 1.5  # proportional gain for the controller
-Kd = 2.0  # derivative gain for the controller
+Kd = 3.0  # derivative gain for the controller
 
 previous_error = target - height  # initial error, in meters
+previous_height = height 
 
 # storage: one list per thing we want to plot
 times = []
@@ -28,9 +29,10 @@ while t < 60.0:
         target = 50.0
     
     error = target - height
-    error_rate = (error - previous_error) / dt
-    throttle = hover + Kp * error + Kd * error_rate
-    previous_error = error
+    height_rate = (height - previous_height) / dt
+    previous_height = height
+    throttle = hover + Kp * error - Kd * height_rate
+    throttle = max(0.0, throttle)  # throttle can't be negative
 
     net_force = throttle - mass * g
     acceleration = net_force / mass
